795e4f9a66d7045983cb2bf3a54c623379051201
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-09-15 14:58:41 -0700

    Completed tasks 2 and 3

--- index.js
+++ index.js
@@ -77,6 +77,9 @@ function Car(model, milesPerGallon) {
   this.tank = 0;
   this.odometer = 0;
 }
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
+};
 
 /*
   TASK 3
@@ -85,7 +88,14 @@ function Car(model, milesPerGallon) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {}
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
